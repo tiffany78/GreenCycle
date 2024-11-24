1f654b19156bcefbbab3de9a9585407d8073dd45
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-11-24 18:37:38 +0700

    implementasi untuk menampilkan tampilan sampah yang dimiliki dan fitur search

--- src/main/java/com/manpro/greencycle/Admin/Storage/StorageController.java
+++ src/main/java/com/manpro/greencycle/Admin/Storage/StorageController.java
@@ -1,16 +1,36 @@
 package com.manpro.greencycle.Admin.Storage;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin/")
 public class StorageController {
 
+    @Autowired
+    JDBCStorageRepository storageRepo;
+
     @GetMapping("/Storage")
-    public String storage() {
-        return "admin/Storage/index"; // Mengacu pada src/main/resources/templates/index.html
+    public String storage(Model model,
+            @RequestParam(defaultValue = "", required =  false) String filter) {
+        
+        List<Storage> sampahList = null;
+        if(filter.length() == 0){
+            sampahList = storageRepo.findAll();
+        }
+        else{
+            sampahList = storageRepo.findWithFilter(filter);
+        }
+
+        model.addAttribute("sampah", sampahList);
+        model.addAttribute("filter", filter);
+        return "admin/Storage/index";
     }
 }
 
