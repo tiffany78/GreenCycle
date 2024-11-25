e8dc0bed9d921fe683f4addd0a8f71a4eab971f7
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-11-25 21:55:49 +0700

    Menampilkan informasi user dari database, menambah view pengguna di SQL, tweak html dan css agar sesuai.

--- src/main/java/com/manpro/greencycle/Admin/Member/MemberController.java
+++ src/main/java/com/manpro/greencycle/Admin/Member/MemberController.java
@@ -1,15 +1,27 @@
 package com.manpro.greencycle.Admin.Member;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin/")
 
 public class MemberController {
+    @Autowired
+    JDBCMemberRepository memberRepository;
+
     @GetMapping("/InformasiMember")
-    public String setoranSampah() {
+    public String setoranSampah(Model model,
+            @RequestParam(defaultValue = "", required = false) String filter) {
+        
+        List<Member> memberList = memberRepository.findAll();
+        model.addAttribute("members", memberList);
         return "admin/InformasiMember/index";
     }
 }
